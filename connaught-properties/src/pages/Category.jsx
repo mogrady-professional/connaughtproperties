c8a0a274a825d561acc3b699d8672c8408b86460
Michael O'Grady <mogrady.professional@gmail.com>
2022-06-14 22:04:57 +0100

    Listing Item Component Styling init

--- connaught-properties/src/pages/Category.jsx
+++ connaught-properties/src/pages/Category.jsx
@@ -12,6 +12,7 @@ import {
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
+import ListingItem from "../components/ListingItem";
 
 import React from "react";
 
@@ -81,7 +82,11 @@ function Category() {
           <main>
             <ul className="categoryListings">
               {listings.map((listing) => (
-                <h3 key={listing.id}>{listing.data.name}</h3>
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
               ))}
             </ul>
           </main>
