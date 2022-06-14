05223ad31e0d6fab4a5ef9bdb84440585f9e5796
Michael O'Grady <mogrady.professional@gmail.com>
2022-06-14 22:24:21 +0100

    Offers Page Init

--- connaught-properties/src/components/Navbar.jsx
+++ connaught-properties/src/components/Navbar.jsx
@@ -50,7 +50,7 @@ function Navbar() {
                   : "navbarListItemName"
               }
             >
-              Offer
+              Offers
             </p>
           </li>
           <li className="navbarListItem">
--- connaught-properties/src/pages/Offers.jsx
+++ connaught-properties/src/pages/Offers.jsx
@@ -1,7 +1,97 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+import ListingItem from "../components/ListingItem";
+
 import React from "react";
 
 function Offers() {
-  return <h1>Offers</h1>;
+  // Set component level state
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+  //   console.log(params);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      const listings = collection(db, "listings");
+
+      try {
+        // Get reference
+        const listingsRef = collection(db, "listings");
+
+        // Query -> look in the url
+        const q = query(
+          listingsRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        // Execute the query
+        const querySnap = await getDocs(q);
+
+        // Assign empty array to listings to push to
+        let listings = [];
+
+        // Loop through the querySnap
+        const result = querySnap.forEach((doc) => {
+          // console.log(doc.data());
+          // Add to listings array
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        toast.error("Could not fetch listings");
+      }
+    };
+    fetchListings();
+  }, []);
+
+  return (
+    <div className="category">
+      <header>
+        <p className="pageHeader">Offers</p>
+      </header>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>There are no current offers</p>
+      )}
+    </div>
+  );
 }
 
 export default Offers;
