55ef59899788f810baa7db2716bc63d440ac6c31
Michael O'Grady <mogrady.professional@gmail.com>
2022-06-14 15:12:08 +0100

    User logout functionality init

--- connaught-properties/src/pages/Profile.jsx
+++ connaught-properties/src/pages/Profile.jsx
@@ -2,18 +2,40 @@ import { useEffect, useState } from "react";
 import { getAuth } from "firebase/auth";
 
 import React from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 function Profile() {
-  const [user, setUser] = useState(null);
-
   const auth = getAuth(); // Get the auth object
 
-  useEffect(() => {
-    console.log(auth.currentUser);
-    setUser(auth.currentUser);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
   });
 
-  return user ? <h1>{user.displayName}</h1> : <h1>Not logged in...</h1>;
+  // Destructure formData
+  const { name, email } = formData;
+
+  // useNavigate is a hook that allows us to navigate to a new page
+  const navigate = useNavigate();
+
+  // Log out from Firebase
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  return (
+    <>
+      <div className="profile">
+        <header className="profileHeader">
+          <p className="pageHeader">My Profile</p>
+          <button type="button" className="logOut" onClick={onLogout}>
+            Logout
+          </button>
+        </header>
+      </div>
+    </>
+  );
 }
 
 export default Profile;
