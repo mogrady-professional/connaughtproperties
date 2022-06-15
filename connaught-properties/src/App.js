281700d30cc7b4c590b77dfdd98c197b0f728a0c
Michael O'Grady <mogrady.professional@gmail.com>
2022-06-15 09:56:43 +0100

    Craete listing init

--- connaught-properties/src/App.js
+++ connaught-properties/src/App.js
@@ -10,6 +10,7 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
 import Category from "./pages/Category";
+import CreateListing from "./pages/CreateListing";
 
 import "./App.css";
 
@@ -29,6 +30,7 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
         <Navbar />
       </Router>
--- connaught-properties/src/pages/Profile.jsx
+++ connaught-properties/src/pages/Profile.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { getAuth, updateProfile } from "firebase/auth";
 import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebase.config";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { arrowRight } from "..assets/svg/keyboardArrowRightIcon.svg";
+import { homeIcon } from "..assets/svg/homeIcon.svg";
 
 function Profile() {
   const auth = getAuth(); // Get the auth object
@@ -98,6 +101,11 @@ function Profile() {
               />
             </form>
           </div>
+          <Link to="/create-listing" className="createListing">
+            <img src={homeIcon} alt="Home" />
+            <p>Sell or rent your home</p>
+            <img src={arrowRight} alt="Arrow Right" />
+          </Link>
         </main>
       </div>
     </>
